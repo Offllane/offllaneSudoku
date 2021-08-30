ea347e894417fa386f11ea0967bc84cf2f1bd877
EXT02D36761 <egor.tsedrik@consultant.turkcell.com.tr>
2021-08-30 12:17:08 +0300

    add unselecting to game service, call it for every new selected cell

--- src/app/main-game/main-game-field/game-cell/game-cell.component.ts
+++ src/app/main-game/main-game-field/game-cell/game-cell.component.ts
@@ -1,5 +1,7 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {CellPosition} from "../../models/cellPosition.interface";
+import {Subscription} from "rxjs";
+import {MainGameService} from "../../main-game.service";
 
 @Component({
   selector: 'app-game-cell',
@@ -7,6 +9,7 @@ import {CellPosition} from "../../models/cellPosition.interface";
   styleUrls: ['./game-cell.component.scss']
 })
 export class GameCellComponent implements OnInit {
+  private dataSubscription = new Subscription();
   public isCurrentCellActive = false;
   @Input() currentValue: number | null = null;
   @Input() cellPosition: CellPosition = {
@@ -14,12 +17,16 @@ export class GameCellComponent implements OnInit {
     columnPosition: 0
   }
 
-  constructor() { }
+  constructor(private mainGameService: MainGameService) { }
 
   ngOnInit(): void {
+    this.dataSubscription.add(this.mainGameService.unselectAllCellsSubject.subscribe(() => {
+      this.isCurrentCellActive = false;
+    }))
   }
 
   selectCell(): void {
+    this.mainGameService.unselectAllCells();
     this.isCurrentCellActive = true;
     console.log(this.cellPosition);
   }
--- src/app/main-game/main-game.service.ts
+++ src/app/main-game/main-game.service.ts
@@ -1,13 +1,18 @@
 import { Injectable } from '@angular/core';
-import {BehaviorSubject} from "rxjs";
+import {BehaviorSubject, Subject} from "rxjs";
 import {gameFieldValues} from "./models/gameFieldValues.type";
 
 @Injectable({
   providedIn: 'root'
 })
 export class MainGameService {
-  private currentGameField: gameFieldValues = new Array(9).fill(0).map(() => new Array(9).fill(null));
+  private currentGameField: gameFieldValues = new Array(9).fill(0).map(() => new Array(9).fill(2));
   public gameFieldValues = new BehaviorSubject(this.currentGameField);
+  public unselectAllCellsSubject = new Subject();
 
   constructor() { }
+
+  public unselectAllCells(): void {
+    this.unselectAllCellsSubject.next();
+  }
 }
